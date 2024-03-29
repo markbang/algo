7358f1d1ed28e4da9ca1b3cbda4a12ac20c19c52
markbang <3137972908@qq.com>
2024-03-29 19:41:56 +0800

    update

--- class_test/random_words.py
+++ class_test/random_words.py
@@ -1,18 +1,18 @@
 import timeit
 
 words = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-def solution(st1, st2):
+def solution(st1, st2): # O(n*log(n))
     if len(st1) == len(st2):
         for i in range(len(st1)):
             if st1[i] not in words or st2[i] not in words:
                 return False
-        li1 = [m for m in st1].sort()
-        li2 = [n for n in st2].sort()
+        li1 = list(st1).sort()
+        li2 = list(st2).sort()
         return li1 == li2
     else:
         return False
 
-def solution2(st1, st2):
+def solution2(st1, st2): # O(n^2)
     if len(st1) == len(st2):
         for i in range(len(st1)):
             if st1[i] not in words or st2[i] not in words:
@@ -24,6 +24,18 @@ def solution2(st1, st2):
         return ''.join(li2) == ''
     else:
         return False
+    
+def solution3(st1, st2): # O(n)
+    dic1 = {}
+    dic2 = {}
+    if len(st1) == len(st2):
+        for i in range(len(st1)):
+            dic1[st1[i]] = dic1.get(st1[i], 0) + 1
+            dic2[st2[i]] = dic2.get(st2[i], 0) + 1
+        return dic1 == dic2
+    else:
+        return False
 if __name__ == '__main__':
-    print(timeit.timeit('solution("abc", "bca")', setup='from __main__ import solution'))
-    print(timeit.timeit('solution2("abc", "bca")', setup='from __main__ import solution2'))
+    print("solution1: ", timeit.timeit("solution('abc', 'bca')", setup="from __main__ import solution"))
+    print("solution2: ", timeit.timeit("solution2('abc', 'bca')", setup="from __main__ import solution2"))
+    print("solution3: ", timeit.timeit("solution3('abc', 'bca')", setup="from __main__ import solution3"))
